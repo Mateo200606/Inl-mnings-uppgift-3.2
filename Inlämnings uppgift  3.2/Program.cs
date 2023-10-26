05e2835960bca69da3a934f9c9f4d6562eeda940
Pingvu <nhlpool27@gmail.com>
2023-10-26 12:54:00 +0200

    Add project files.

---
+++ Inlämnings uppgift  3.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace inlämnings_uppgift_3_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet svara J eller N");
+            string svar = (Console.ReadLine());
+            Console.WriteLine("Hur gammal är du?");
+            int svar1 = int.Parse(Console.ReadLine());
+            if (svar1 <= 22 && svar1 >= 19)
+            {
+                Console.WriteLine("Vi vill gärna anställa dig");
+
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvär efter annan personal just nu");
+
+            }
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
